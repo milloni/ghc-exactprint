047e5fb78959932814940960d236f5c2549aa02e
Max Tomago <noreply@tomagotech.com>
2022-09-20 17:23:16 +0100

    ghc b91798be

--- src/Language/Haskell/GHC/ExactPrint/Utils.hs
+++ src/Language/Haskell/GHC/ExactPrint/Utils.hs
@@ -212,10 +212,6 @@ insertCppComments (L l p) cs = L l p'
 -- ---------------------------------------------------------------------
 
 ghcCommentText :: LEpaComment -> String
-ghcCommentText (L _ (GHC.EpaComment (EpaDocCommentNext s) _))  = s
-ghcCommentText (L _ (GHC.EpaComment (EpaDocCommentPrev s) _))  = s
-ghcCommentText (L _ (GHC.EpaComment (EpaDocCommentNamed s) _)) = s
-ghcCommentText (L _ (GHC.EpaComment (EpaDocSection _ s) _))    = s
 ghcCommentText (L _ (GHC.EpaComment (EpaDocOptions s) _))      = s
 ghcCommentText (L _ (GHC.EpaComment (EpaLineComment s) _))     = s
 ghcCommentText (L _ (GHC.EpaComment (EpaBlockComment s) _))    = s
